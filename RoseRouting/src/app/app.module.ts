51d57c4810e291a24e1b621010748bcb6a60b859
Coolone36 <michaeljlaritz@gmail.com>
2017-10-22 12:33:26 -0400

    Added AM2, Flex Layout setup, updated angular dependencies, did some styling, added toolbar to webpage to test

--- RoseRouting/src/app/app.module.ts
+++ RoseRouting/src/app/app.module.ts
@@ -6,6 +6,65 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './+home/home.component';
 import { SigninComponent } from './+signin/signin.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// This is deprecated because it imports everything and adds loading time to webpage.
+// Current Recommended apporach is add all individually and remove ones that are not used.
+// import { MaterialModule } from '@angular/material';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from '@angular/material';
+
+export const MaterialModules = [
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+];
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +74,9 @@ import { SigninComponent } from './+signin/signin.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModules,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
