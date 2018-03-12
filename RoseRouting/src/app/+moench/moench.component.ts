15591a847f28ffaea401c0698b4b5f870b604582
Coolone36 <michaeljlaritz@gmail.com>
2018-03-12 01:46:54 -0400

    Added displayed images for moench floors

--- RoseRouting/src/app/+moench/moench.component.ts
+++ RoseRouting/src/app/+moench/moench.component.ts
@@ -6,21 +6,23 @@ import { ActivatedRoute, Params } from '@angular/router';
   templateUrl: './moench.component.html',
   styleUrls: ['./moench.component.scss']
 })
-export class MoenchComponent implements OnInit, OnDestroy{
-  message = "Welcome to Moench";
+export class MoenchComponent implements OnInit, OnDestroy {
+  message = 'Welcome to Moench';
+  imageUrl = '../assets/images/moench100.png';
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    document.getElementById('moench-btn').classList.add("active");
-    this.route.params.subscribe((routeParams: Params) => { 
-      const floor = routeParams["floor"];
+    document.getElementById('moench-btn').classList.add('active');
+    this.route.params.subscribe((routeParams: Params) => {
+      const floor = routeParams['floor'];
       this.message = `Moench ${floor}`;
-    })
+      this.imageUrl = `../assets/images/moench${floor}.png`;
+    });
   }
 
   ngOnDestroy() {
-    document.getElementById('moench-btn').classList.remove("active");
+    document.getElementById('moench-btn').classList.remove('active');
   }
 
 }
